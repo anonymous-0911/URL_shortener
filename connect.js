7eb3f464ac241dd485acd309cc32a396ebf143b8
Harsh <hrshzz89@gmail.com>
2026-09-13 12:26:59 +0530

    add mongodb connection timeouts

--- connect.js
+++ connect.js
@@ -4,7 +4,10 @@ let cached = null;
 
 async function connectToMongoDB(url) {
   if (cached) return cached;
-  cached = await mongoose.connect(url);
+  cached = mongoose.connect(url, {
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 10000,
+  });
   return cached;
 }
 
